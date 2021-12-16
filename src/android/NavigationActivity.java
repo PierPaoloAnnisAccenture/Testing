36d42c91f410643ba8b2d84cfc7a62f20263d7a3
Felice Simone <felice.simone@accenture.com>
2021-12-16 10:50:23 +0100

    test new activity

--- src/android/NavigationActivity.java
+++ src/android/NavigationActivity.java
@@ -1,15 +1,32 @@
 package $appid;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import $appid.R;
+
+import com.outsystems.bluegps.BlueGPS;
+import com.synapseslab.bluegps_sdk.data.model.map.GenericResource;
+import $appid.databinding.ActivityNavigationBinding;
 
 public class NavigationActivity extends AppCompatActivity {
+
+
+    private String TAG = "NavigationActivity";
+    private ActivityNavigationBinding binding;
+    private View toolbarView;
+    private GenericResource source;
+    private GenericResource destination;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+        binding = ActivityNavigationBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        getSupportActionBar().setTitle("Navigation View");
+        binding.webView.initMap(BlueGPS.sdkEnvironment, BlueGPS.configurationMap, null);
     }
 }
