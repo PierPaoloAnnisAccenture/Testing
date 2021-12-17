d14f24daff46841c03c6012cc6de7546aff6f966
Felice Simone <felice.simone@accenture.com>
2021-12-17 11:12:28 +0100

    test new activity

--- src/android/NavigationActivity.java
+++ src/android/NavigationActivity.java
@@ -2,7 +2,7 @@ package $appid;
 
 import android.os.Bundle;
 import android.view.View;
-
+import java.util.ArrayList;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,14 +10,17 @@ import com.outsystems.bluegps.BlueGPS;
 import com.synapseslab.bluegps_sdk.data.model.map.GenericResource;
 import $appid.databinding.ActivityNavigationBinding;
 
+import com.synapseslab.bluegps_sdk.data.model.map.ConfigurationMap;
+import com.synapseslab.bluegps_sdk.data.model.map.IconStyle;
+import com.synapseslab.bluegps_sdk.data.model.map.MapStyle;
+import com.synapseslab.bluegps_sdk.data.model.map.NavigationStyle;
+import com.synapseslab.bluegps_sdk.data.model.map.ShowMap;
+
 public class NavigationActivity extends AppCompatActivity {
 
     private ActivityNavigationBinding binding;
-    private String TAG = "NavigationActivity";
-    private View toolbarView;
-    private GenericResource source;
-    private GenericResource destination;
-
+    private ArrayList<GenericResource> genericResourceList = new ArrayList<>();
+    private ConfigurationMap configurationMap = setupConfigurationMap();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,6 +28,34 @@ public class NavigationActivity extends AppCompatActivity {
         binding = ActivityNavigationBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-        getSupportActionBar().setTitle("Navigation View");
+//        binding.webView.initMap(BlueGPS.sdkEnvironment, configurationMap, null);
+    }
+
+    private ConfigurationMap setupConfigurationMap() {
+        ConfigurationMap configurationMap = new ConfigurationMap();
+
+        MapStyle mapStyle = new MapStyle();
+
+        NavigationStyle navigationStyle = new NavigationStyle();
+        navigationStyle.setIconSource("/api/public/resource/icons/commons/start.svg");
+        navigationStyle.setIconDestination("/api/public/resource/icons/commons/end.svg");
+
+        IconStyle iconStyle = new IconStyle();
+        iconStyle.setName("saipem");
+        iconStyle.setAlign("center");
+        iconStyle.setVAlign("center");
+        iconStyle.setFollowZoom(false);
+
+        mapStyle.setNavigation(navigationStyle);
+        mapStyle.setIcons(iconStyle);
+
+        ShowMap showMap = new ShowMap();
+        showMap.setAll(false);
+        showMap.setRoom(true);
+        showMap.setMe(true);
+
+        configurationMap.setStyle(mapStyle);
+        configurationMap.setShow(showMap);
+        return configurationMap;
     }
 }
